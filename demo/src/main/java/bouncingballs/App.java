484cc0f02f62fec3d2700d892a5d2c40732d5b07
Dev B <devb@bu.edu>
2024-02-23 21:19:24 -0500

    Cleanup

--- demo/src/main/java/bouncingballs/App.java
+++ demo/src/main/java/bouncingballs/App.java
@@ -26,12 +26,22 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-    Pane canvas = new Pane();
-    Scene scene = new Scene(canvas, 1300, 800);
-    ArrayList<Circle> balls = new ArrayList<Circle>(); 
-    ArrayList<Integer> counts = new ArrayList<Integer>();
+    private Pane canvas = new Pane();
+    private Scene scene = new Scene(canvas, 1300, 800);
+    private ArrayList<Circle> balls = new ArrayList<Circle>(); 
+    private ArrayList<Boolean> toFlip = new ArrayList<Boolean>();
     private final int numBalls = 20;
+    private double mouseX;
+    private double mouseY;
 
+    /* 
+     * A function to handle creating the timeline of a single Circle
+     * IN:  Circle object
+     *      double dx
+     *      double dy
+     *      int index
+     * OUT: void
+    */
     public void handleTimeline(Circle ball, double dx, double dy, final int index){
         
         Timeline loop = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
@@ -54,10 +64,10 @@ public class App extends Application {
                 if (atBottomBorder || atTopBorder) {
                     deltaY *= -1;
                 }
-                if (counts.get(index) % 2 == 1){
+                if (toFlip.get(index)){
                     deltaX *= -1;
                     deltaY *= -1;
-                    counts.set(index, counts.get(index) + 1);
+                    toFlip.set(index, false);
                 }
             }
         }));
@@ -66,27 +76,47 @@ public class App extends Application {
         loop.play();
     }
 
+    /* 
+     * A function to randomly generate a ball's radius
+     * IN:  Void
+     * OUT: double radius 
+    */
     private double generateRadius(){
         Random rand = new Random();
         return rand.nextInt(40 - 10) + 10;
     }
 
+    /* 
+     * A function to randomly generate a ball's color
+     * IN:  Void
+     * OUT: Color object
+    */
     private Color generateColor(){
         Random rand = new Random();
         return Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
     }
 
-    public double calculateDelta(Circle ball){
+    /* 
+     * A function to calculate the radius of an object based on its radius
+     * IN:  Circle object
+     * OUT: double delta 
+    */
+    public double calculateInitialDelta(Circle ball){
         return (1 / ball.getRadius()) * 10 ;
     }
 
+    /* 
+     * A function to initialize all of the variables and kick off the timelines
+     * IN:  Stage object
+     * OUT: void
+    */
     @Override
     public void start(Stage stage) {        
         for(int i = 0; i < numBalls; i++){
             balls.add(new Circle(generateRadius(), generateColor()));
             balls.get(i).relocate(i*30, i*30);
             canvas.getChildren().add(balls.get(i));
-            counts.add(0);
+            toFlip.add(false);
         }
         
         stage.setTitle("Moving Ball");
@@ -94,17 +124,17 @@ public class App extends Application {
         stage.show();
         
         for(int i = 0; i < numBalls; i++){
-            handleTimeline(balls.get(i), calculateDelta(balls.get(i)), calculateDelta(balls.get(i)), i);
+            handleTimeline(balls.get(i), calculateInitialDelta(balls.get(i)), calculateInitialDelta(balls.get(i)), i);
         }
 
         scene.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
             @Override
             public void handle(javafx.scene.input.MouseEvent event) {
-                double mouseX = event.getX();
-                double mouseY = event.getY();
+                mouseX = event.getX();
+                mouseY = event.getY();
                 // Move circles away from the mouse click
                 for(int i = 0; i < numBalls; i++){
-                    counts.set(i, counts.get(i) + 1);
+                    toFlip.set(i, true);
                 }
             }
         });
